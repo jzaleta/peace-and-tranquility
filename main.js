ec5f558f02af483f2c3db9725f80e5ea3c70ac9b
Javier Zaleta Martínez <94091554+jzaleta@users.noreply.github.com>
2023-07-14 16:47:48 -0600

    feat: finish main.js

--- main.js
+++ main.js
@@ -46,3 +46,9 @@ window.onload = () => {
     start();
   }
 };
+
+console.log(
+  "%c Created by Javier Zaleta Martínez ",
+  "color: #fff; background: hsl(0, 93%, 45%); padding: 5px 0;"
+);
+console.log();
