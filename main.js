df2a78d3c3772646108a74eb0f47f11e3d840de5
Javier Zaleta Martínez <94091554+jzaleta@users.noreply.github.com>
2023-08-09 13:14:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ import { dancin, start, updatePhrase } from './dancin.js';
 
 // Notes so I don't forget what does this code do:
 
-const containerDiv = document.createElement('div');
-containerDiv.className = 'corner';
+const cornerDiv = document.createElement('div');
+cornerDiv.className = 'corner';
 
 // Create a div with class "container"
 const containerDiv = document.createElement('div');
